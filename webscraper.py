7588c01f23e70cf40ce8a20da7fff96c4a612c9f
imia307 <imia307@aucklanduni.ac.nz>
2023-12-14 01:24:41 +1300

    changed the gpu being searched

--- webscraper.py
+++ webscraper.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-url = 'https://www.pbtech.co.nz/product/VGAGBV44093/Gigabyte-NVIDIA-GeForce-RTX-4090-AERO-OC-24GB-GDDR'
+url = 'https://www.pbtech.co.nz/product/VGAGLX040910/GALAX-NVIDIA-GeForce-RTX-4090-ST-24GB-GDDR6X-Graph'
 browser = webdriver.Chrome()
 browser.get(url)
 
 name = browser.find_element('xpath','//*[@id="main_container"]/div[3]/div/div[2]/div[1]/div/div[1]/h1')
-price =  browser.find_element('xpath','//*[@id="main_container"]/div[3]/div/div[2]/div[1]/div/div[2]/div[1]/div[1]/div[2]/div/div[1]/div/span[3]/span/span[2]') 
+price =  browser.find_element('xpath','//*[@id="main_container"]/div[3]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div/span[3]/span/span[2]') 
 
 
 list = [name.text]

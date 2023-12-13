2b1426690c34760143c3dec34e8ceff13fd3eb11
imia307 <imia307@aucklanduni.ac.nz>
2023-12-14 02:39:14 +1300

    figured out how to access data from the modal where it shows pb tech item stock

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from datetime import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
@@ -10,13 +14,21 @@ browser.get(url)
 name = browser.find_element('xpath','//*[@id="main_container"]/div[3]/div/div[2]/div[1]/div/div[1]/h1')
 price =  browser.find_element('xpath','//*[@id="main_container"]/div[3]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div/span[3]/span/span[2]') 
 
-
 list = [name.text]
 print(list)
 
+
+
+available_modal = browser.find_element(By.LINK_TEXT, "Check store stock levels").click()
+time.sleep(10)
+available = browser.find_element('xpath','//*[@id="js-desktop-stock-table-container"]/table/tbody/tr[19]/td[2]')
+l = [available.text]
+print(l)
+
+
 file = open("GpuData.txt", "w")
 file.write(list[0])
-file.write(' Price: $'+price.text + ' Time: '+formatted_datetime)
+file.write(' Price: $'+ price.text + ' Time: '+ formatted_datetime)
 file.close()
 
 #test commit
